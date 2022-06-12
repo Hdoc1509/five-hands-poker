40a6a9f563f554d5ca378011cba1b728bc17cc79
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-12 12:58:07 -0500

    Changed functions to arrow functions in game-rules.js

--- src/js/game-rules.js
+++ src/js/game-rules.js
@@ -1,22 +1,16 @@
-/**
-
-  TODO: apply changes of class names inside modal
-
- */
-
 const dialogOpenBtn = document.querySelector('.dialog-open-button');
 const dialogRules = document.querySelector('.dialog-rules');
 const dialogCloseBtn = document.querySelector('.dialog-rules__close');
 
-dialogOpenBtn.addEventListener('click', showRules);
+const hideRules = () => {
+  dialogRules.close();
+  dialogOpenBtn.classList.remove('hidden');
+};
 
-function showRules() {
+const showRules = () => {
   dialogRules.showModal();
   dialogOpenBtn.classList.add('hidden');
   dialogCloseBtn.addEventListener('click', hideRules);
-}
+};
 
-function hideRules() {
-  dialogRules.close();
-  dialogOpenBtn.classList.remove('hidden');
-}
+dialogOpenBtn.addEventListener('click', showRules);
