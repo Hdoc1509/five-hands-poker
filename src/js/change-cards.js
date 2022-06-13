f7b03ca1c66ba09a12fb5276fe6152f7211a49ec
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-13 12:33:15 -0500

    Removed unnecesary jsdoc comments in change-cards.js

--- src/js/change-cards.js
+++ src/js/change-cards.js
@@ -1,53 +1,48 @@
-/**
- * Change card button
- * @type {HTMLElement}
- */
 const changeButton = document.getElementById('change-button');
 
-/** Change selected cards */
 function changeSelectedCards() {
-	const selectedCards = document.querySelectorAll('.card--selected'),
-		len = selectedCards.length;
+  const selectedCards = document.querySelectorAll('.card--selected'),
+    len = selectedCards.length;
 
-	function changeCardsQuantity(quantity) {
-		for (let i = 0; i < quantity; i++) {
-			let card = setCardNumAndFig(selectedCards[i].id, true),
-				cardPosition = setNewCardPosition(
-					selectedCards[i].id,
-					card.number,
-					card.figure
-				);
+  function changeCardsQuantity(quantity) {
+    for (let i = 0; i < quantity; i++) {
+      let card = setCardNumAndFig(selectedCards[i].id, true),
+        cardPosition = setNewCardPosition(
+          selectedCards[i].id,
+          card.number,
+          card.figure
+        );
 
-			selectedCards[
-				i
-			].firstElementChild.innerHTML = `${card.number}<br>${card.figure}`;
-			setCardColor(card.figure, cardPosition);
-		}
+      selectedCards[
+        i
+      ].firstElementChild.innerHTML = `${card.number}<br>${card.figure}`;
+      setCardColor(card.figure, cardPosition);
+    }
 
-		// Updating the counter of remaining cards
-		remainingCardsCounter -= quantity;
-	}
+    // Updating the counter of remaining cards
+    remainingCardsCounter -= quantity;
+  }
 
-	if (len > remainingCardsCounter) {
-		errChangeCardsQuantity();
-	} else if (len == 0) {
-		errNoCardSelected();
-	} else {
-		changeCardsQuantity(len);
-	}
+  if (len > remainingCardsCounter) {
+    errChangeCardsQuantity();
+  } else if (len == 0) {
+    errNoCardSelected();
+  } else {
+    changeCardsQuantity(len);
+  }
 
-	selectedCards.forEach((card) => {
-		card.classList.remove('card--selected');
-	});
+  selectedCards.forEach((card) => {
+    card.classList.remove('card--selected');
+  });
 
-	// Updating the counter of remaining cards
-	remainingCards.innerHTML = `${remainingCardsCounter}`;
+  // Updating the counter of remaining cards
+  remainingCards.innerHTML = `${remainingCardsCounter}`;
 
-	if (remainingCardsCounter == 0) {
-		hideShowButton(changeButton, 'hide');
+  if (remainingCardsCounter == 0) {
+    hideShowButton(changeButton, 'hide');
 
-		for (let card of cardsToChange) {
-			card.removeEventListener('click', toggleClassSelectedCard);
-		}
-	}
+    for (let card of cardsToChange) {
+      card.removeEventListener('click', toggleClassSelectedCard);
+    }
+  }
 }
