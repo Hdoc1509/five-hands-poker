278cbca0ffcdb61023c125cc06044f824bc103c7
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-14 19:02:17 -0500

    Refactor(start-game.js): Simplified logic to generate first 5 cards of the hand by removing setCardColor()

--- src/js/start-game.js
+++ src/js/start-game.js
@@ -18,12 +18,10 @@ function startGame() {
 
   // Generating the first 5 cards of the hand
   for (let i = 0; i < 5; i++) {
-    const { id, figure, cardInfo } = setCardNumAndFig(`card${i + 1}`, false);
+    const { id, cardInfo } = setCardNumAndFig(`card${i + 1}`, false);
 
-    // Adding to each card its info on its corners
-    setCardColor(figure, i + 1);
+    // Displaying card info
     cardsInfo[i].innerText = cardInfo;
-
     if (id === 'card5') cardsInfo[i + 1].innerText = cardInfo;
   }
 
