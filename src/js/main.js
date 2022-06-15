352c327db28717bc6e13767b393306379e6d2f30
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-06-15 11:49:59 -0500

    (main.js): Removed unused variable remainingCardsContainer

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,6 @@ const cardDisplay = document.querySelectorAll('.card__figure');
 /** Card containers */
 const cardsToChange = document.querySelectorAll('.card');
 
-const remainingCardsContainer = document.getElementById(
-  'remaining-cards-container'
-);
-
 const figures = ['♥', '♦', '♣', '♠'];
 
 const numbers = ['A', 2, 3, 4, 5, 6, 7, 8, 9, '10', 'J', 'Q', 'K'];
