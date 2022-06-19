5ca32a42041dbc3c1d61923b328fab6ecf1cb8ba
hdoc <blueguitarist1520@gmail.com>
2022-06-19 13:14:43 -0500

    Feat(poker-hands.js): Now isStraight() can check if hand is any of the possible STRAIGHTS

--- src/js/poker-hands.js
+++ src/js/poker-hands.js
@@ -65,10 +65,17 @@ const isTwoPair = (cards, [{ number: number1 }, { number: number2 }]) => {
  * Check if hand is an specific STRAIGHT
  * @param  {Array<ObjCard>} cards
  * @param  {string} straight
+ * @returns {boolean}
  */
-const isStraight = (cards, straight) =>
-  cards.every(({ number }) => number.match(STRAIGHT[straight])) &&
-  allDifferentNumbers(cards);
+const isStraight = (cards, straight) => {
+  if (straight == null)
+    return Object.keys(STRAIGHT).some((key) => isStraight(cards, key));
+
+  return (
+    cards.every(({ number }) => number.match(STRAIGHT[straight])) &&
+    allDifferentNumbers(cards)
+  );
+};
 
 /**
  * Check if hand is an specific THREE OF A KIND
