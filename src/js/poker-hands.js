270e4591fbcb34210bd1f34a5700a3b32ea2b28d
hdoc <blueguitarist1520@gmail.com>
2022-06-22 17:52:31 -0500

    Feat(poker-hands.js): Added new functions isAnyFullHouse() and findFullHouse() and new jsdoc typedef FullHouseData

--- src/js/poker-hands.js
+++ src/js/poker-hands.js
@@ -213,6 +213,37 @@ const isFullHouse = (cards, [{ number: number1 }, { number: number2 }]) => {
 };
 
 /**
+ * Check if hand is any possible FULL HOUSE
+ * @param {Array<ObjCard>} cards
+ */
+const isAnyFullHouse = (cards) => {
+  const pairs = getPairs(cards);
+  const threeOfKind = getThreeOfKind(cards);
+
+  return pairs.size === 1 && threeOfKind.size === 1;
+};
+
+/**
+ * Full House data
+ * @typedef {Object} FullHouseData
+ * @property {string} pair
+ * @property {string} threeOfKind
+ */
+
+/**
+ * Retrieves the numbers that compose the FULL HOUSE in the hand
+ * @param {Array<ObjCard>} cards
+ *
+ * @return {FullHouseData}
+ */
+const findFullHouse = (cards) => {
+  const [pair] = Array.from(getPairs(cards));
+  const [threeOfKind] = Array.from(getThreeOfKind(cards));
+
+  return { pair, threeOfKind };
+};
+
+/**
  * Check if hand is an specific FOUR OF A KIND
  * @param {Array<ObjCard>} cards
  * @param {ObjCard}        card        - Card with specific number
