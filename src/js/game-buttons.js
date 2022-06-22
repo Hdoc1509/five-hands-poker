01651d605f94c7e8ff5ecdd358959b6b8b3f78fa
hdoc <blueguitarist1520@gmail.com>
2022-06-22 18:23:05 -0500

    Add(game-buttons.js): Added new module for game buttons

---
+++ src/js/game-buttons.js
@@ -0,0 +1,7 @@
+export const GAME_BUTTONS = Object.freeze({
+  start: document.getElementById('start-button'),
+  change: document.getElementById('change-button'),
+  stay: document.getElementById('stay-button'),
+  nextHand: document.getElementById('next-hand-button'),
+  playAgain: document.getElementById('play-again-button'),
+});
