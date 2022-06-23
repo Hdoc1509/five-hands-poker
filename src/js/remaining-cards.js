9033657d26d9175595d35c8afd57ffea426ff352
hdoc <blueguitarist1520@gmail.com>
2022-06-23 18:27:31 -0500

    Feat(remaining-cards.js): setRemainingCardsCounter() now can receive a number, null or a callback as argument

--- src/js/remaining-cards.js
+++ src/js/remaining-cards.js
@@ -4,8 +4,8 @@ import { gid } from './utils/dom.js';
 let remainingCardsCounter = null;
 
 /**
- * Sets new value for counter of remaining cards
- * @param  {Function} callback Callback for update counter of remaining cards
+ * Sets and display new value for counter of remaining cards
+ * @param  {number|Function} data Callback or value for update counter of remaining cards
  *
  * @example
  * // Increments the value in 1
@@ -16,10 +16,27 @@ let remainingCardsCounter = null;
  * setRemainingCardsCounter((value) => value - myNum);
  * @example
  * // Sets the value to 3
- * setRemainingCardsCounter((value) => 3);
+ * setRemainingCardsCounter(3);
+ * @example
+ * // Sets the value to null
+ * setRemainingCardsCounter(null);
  */
-export const setRemainingCardsCounter = (callback) => {
-  remainingCardsCounter = callback(remainingCardsCounter);
+export const setRemainingCardsCounter = (data) => {
+  let aux = null;
+
+  if (typeof data === 'function') {
+    aux = data(remainingCardsCounter);
+
+    if (typeof aux !== 'number')
+      throw new TypeError('Returned value must be a number');
+  } else {
+    aux = data;
+
+    if (typeof aux !== 'number' && aux !== null)
+      throw new TypeError('Expected a number or null as argument');
+  }
+
+  remainingCardsCounter = aux;
   gid('remaining-cards').textContent = remainingCardsCounter?.toString() ?? '-';
 };
 
