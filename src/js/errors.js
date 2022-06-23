cdd48fe087d332c3a24267237ec93ea079ebf2ae
hdoc <blueguitarist1520@gmail.com>
2022-06-23 12:13:20 -0500

    Feat(error.js): Modularized logic for game errors

--- src/js/errors.js
+++ src/js/errors.js
@@ -1,7 +1,7 @@
-function errChangeCardsQuantity() {
-	alert("Error. No se puede cambiar esa cantidad de cartas");
-}
+export const changeCardsQuantityError = () => {
+  alert('Error. No se puede cambiar esa cantidad de cartas');
+};
 
-function errNoCardSelected() {
-	alert("Error. Debes elegir una carta");
-}
+export const noCardSelectedError = () => {
+  alert('Error. Debes elegir una carta');
+};
