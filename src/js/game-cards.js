d57db2d2764ffd93b526b4cc9e69063d6da19be3
hdoc <blueguitarist1520@gmail.com>
2022-06-23 11:47:51 -0500

    Add(game-cards.js): Added new module for game cards logic

---
+++ src/js/game-cards.js
@@ -0,0 +1,72 @@
+import { cleanArray } from './utils/array.js';
+
+/** @type {Array<String>} */
+const gameCards = [];
+
+/**
+ * Card Object
+ * @typedef  {Object} ObjCard
+ * @property {String} id
+ * @property {String} number
+ * @property {String} figure
+ */
+
+/**
+ * Array of cards for each hand
+ * @type {Array<ObjCard>}
+ */
+export const generatedObjCards = [];
+
+const figures = ['♥', '♦', '♣', '♠'];
+const numbers = [
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+];
+
+export const getObjCards = () => generatedObjCards;
+
+export const cleanObjCards = () => cleanArray(generatedObjCards);
+
+export const getRandomCard = () =>
+  gameCards[Math.floor(Math.random() * gameCards.length)];
+
+/**
+ * @param {string} card - Card to remove
+ */
+export const removeCard = (card) => {
+  const cardPosition = gameCards.indexOf(card);
+  gameCards.splice(cardPosition, 1);
+};
+
+// Generating all game cards
+((gameCards) => {
+  const getRandomNumber = () =>
+    numbers[Math.floor(Math.random() * numbers.length)];
+
+  const getRandomFigure = () =>
+    figures[Math.floor(Math.random() * figures.length)];
+
+  const generateRandomCard = () => {
+    let card = `${getRandomNumber()}${getRandomFigure()}`;
+
+    /** Validation for get unique cards */
+    while (gameCards.includes(card))
+      card = `${getRandomNumber()}${getRandomFigure()}`;
+
+    return card;
+  };
+
+  // Generating all cards
+  for (let i = 0; i < 52; i++) gameCards.push(generateRandomCard());
+})(gameCards);
