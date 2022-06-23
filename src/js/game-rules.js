a45ef7123785cc7926fa3650dfb03e9d8a2c02e7
hdoc <blueguitarist1520@gmail.com>
2022-06-23 12:24:23 -0500

    Refactor(game-rules.js): Added prefix $ to all DOM variables

--- src/js/game-rules.js
+++ src/js/game-rules.js
@@ -1,17 +1,18 @@
-const dialogOpenBtn = document.querySelector('.dialog-open-button');
+const $dialogOpenBtn = document.querySelector('.dialog-open-button');
+const $dialogCloseBtn = document.querySelector('.dialog-rules__close');
+
 /** @type {HTMLDialogElement} */
-const dialogRules = document.querySelector('.dialog-rules');
-const dialogCloseBtn = document.querySelector('.dialog-rules__close');
+const $dialogRules = document.querySelector('.dialog-rules');
 
 const hideRules = () => {
-  dialogRules.close();
-  dialogOpenBtn.classList.remove('hidden');
+  $dialogRules.close();
+  $dialogOpenBtn.classList.remove('hidden');
 };
 
 const showRules = () => {
-  dialogRules.showModal();
-  dialogOpenBtn.classList.add('hidden');
-  dialogCloseBtn.addEventListener('click', hideRules);
+  $dialogRules.showModal();
+  $dialogOpenBtn.classList.add('hidden');
+  $dialogCloseBtn.addEventListener('click', hideRules);
 };
 
-dialogOpenBtn.addEventListener('click', showRules);
+$dialogOpenBtn.addEventListener('click', showRules);
