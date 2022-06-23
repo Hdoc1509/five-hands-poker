755de3d6f9c3ee97bc4425b79289ac6aa9f4bdc3
hdoc <blueguitarist1520@gmail.com>
2022-06-23 11:42:28 -0500

    Add(game-hands.js): Added new module for game hands counter logic

---
+++ src/js/game-hands.js
@@ -0,0 +1,33 @@
+/** @type {number} Counter of hands player */
+let playerHandsCounter = null;
+
+/**
+ * Sets new value for counter of player hands
+ * @param  {Function} callback Callback for update counter of player hands
+ *
+ * @example
+ * // Increments the value in 1
+ * setRemainingCards((value) => value + 1);
+ * @example
+ * // Decrements the value in value of myNum
+ * const myNum = 2;
+ * setRemainingCards((value) => value - myNum);
+ * @example
+ * // Sets the value to 3
+ * setRemainingCards(() => 3);
+ */
+export const setPlayerHandsCounter = (callback) => {
+  playerHandsCounter = callback(playerHandsCounter);
+};
+
+/** Gets the current value of counter of player hands */
+export const getPlayerHandsCounter = () => playerHandsCounter;
+
+/** Remove class "section__current-hand" */
+export const clearCurrentHandClass = () => {
+  const $currentHand = document.querySelector(
+    'p.points-details__hand--current'
+  );
+
+  $currentHand?.classList.remove('points-details__hand--current');
+};
