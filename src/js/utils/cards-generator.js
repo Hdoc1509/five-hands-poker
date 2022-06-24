921ef4df8713a305eba36bc78e35899c3b2c0f9e
hdoc <blueguitarist1520@gmail.com>
2022-06-24 11:50:01 -0500

    Add(cards-generator.js): Added new util module cards-generator.js

---
+++ src/js/utils/cards-generator.js
@@ -0,0 +1,46 @@
+const suits = ['♥', '♦', '♣', '♠'];
+const numbers = [
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+];
+
+const getRandomNumber = () =>
+  numbers[Math.floor(Math.random() * numbers.length)];
+
+const getRandomSuit = () => suits[Math.floor(Math.random() * suits.length)];
+
+const getRandomCard = () => `${getRandomNumber()}${getRandomSuit()}`;
+
+export const generateCards = (quantity = 52) => {
+  if (typeof quantity !== 'number')
+    throw new TypeError('Expected a number as argument.');
+
+  if (quantity > 52 || quantity < 1)
+    throw new RangeError('Argument must be between 1 and 52.');
+
+  /** @type {Set<string>} */
+  const cards = new Set();
+
+  const generateCard = () => {
+    let card = getRandomCard();
+
+    while (cards.has(card)) card = getRandomCard();
+
+    return card;
+  };
+
+  for (let i = 0; i < quantity; i++) cards.add(generateCard());
+
+  return Array.from(cards);
+};
