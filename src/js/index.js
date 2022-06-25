396f048302a72ef25194e13f9d19e40204f71a88
hdoc <blueguitarist1520@gmail.com>
2022-06-25 17:54:21 -0500

    Feat(index.js): Added listener option once true for GAME_BUTTONS.start

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,5 @@ import { GAME_BUTTONS } from './game-buttons.js';
 import { setPlayerHandsCounter } from './game-hands.js';
 import { startGame } from './start-game.js';
 
-GAME_BUTTONS.start.addEventListener('click', startGame);
+GAME_BUTTONS.start.addEventListener('click', startGame, { once: true });
 setPlayerHandsCounter(() => 1);
