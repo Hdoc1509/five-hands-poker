147a4ebd11633332679c41a12a8d8d91db957709
hdoc <blueguitarist1520@gmail.com>
2022-06-25 18:20:11 -0500

    Docs(game-buttons.js): Added jsdoc comments for GAME_BUTTONS

--- src/js/game-buttons.js
+++ src/js/game-buttons.js
@@ -1,5 +1,18 @@
 import { gid } from './utils/dom.js';
 
+/**
+ * @typedef  {Object}            GameButtons
+ * @property {HTMLButtonElement} start
+ * @property {HTMLButtonElement} change
+ * @property {HTMLButtonElement} stay
+ * @property {HTMLButtonElement} nextHand
+ * @property {HTMLButtonElement} playAgain
+ */
+
+/**
+ * Game buttons
+ * @type {GameButtons}
+ */
 export const GAME_BUTTONS = Object.freeze({
   start: gid('start-button'),
   change: gid('change-button'),
