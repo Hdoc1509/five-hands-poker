015d6e145ef7e1cabd824842e03b68ef2e4fe00f
hdoc <blueguitarist1520@gmail.com>
2022-06-24 19:02:14 -0500

    Feat(game-cards.js): Added new function setObjCards()

--- src/js/game-cards.js
+++ src/js/game-cards.js
@@ -19,6 +19,51 @@ const gameCards = generateCards();
  */
 export const generatedObjCards = [];
 
+/**
+ * Callbak for update object cards
+ * @callback ObjCardCallback
+ * @param {Array<ObjCard>} cards Current array of object cards
+ * @returns {Array<ObjCard>} New array of object cards
+ */
+
+/**
+ * Sets new value for object cards of each hand
+ * @param  {Array<ObjCard>|ObjCardCallback} data Callback or value for update object cards
+ *
+ * @example
+ * // Edit hand at index 2
+ * setObjCards((cards) => {
+ *   cards[2] = { number: '5', figure: '♦'};
+ *
+ *  return cards;
+ * });
+ * @example
+ * // Add a new card
+ * const card = { number: '9', figure: '♣'};
+ * setObjCards((cards) => [...cards, card]);
+ * @example
+ * // Clean cards
+ * setObjCards([]);
+ */
+export const setObjCards = (data) => {
+  let aux = null;
+
+  if (typeof data === 'function') {
+    aux = data(generatedObjCards.map((card) => card));
+
+    if (!Array.isArray(aux))
+      throw new TypeError('Returnred value must be an array');
+  } else {
+    aux = data;
+
+    if (!Array.isArray(aux))
+      throw new TypeError('Expected an array as argument');
+  }
+
+  cleanArray(generatedObjCards);
+  generatedObjCards.push(...aux);
+};
+
 export const getObjCards = () => generatedObjCards;
 
 export const cleanObjCards = () => cleanArray(generatedObjCards);
@@ -29,5 +74,6 @@ export const getRandomCard = () => {
 
   // Removing card
   gameCards.splice(cardPosition, 1);
+
   return parseCard(card);
 };
