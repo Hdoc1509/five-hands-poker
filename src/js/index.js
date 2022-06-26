cc25d0b53961b174009f80950f3a3483a591388f
hdoc <blueguitarist1520@gmail.com>
2022-06-26 18:13:09 -0500

    Fix(index.js): Fixed relative paths of imported modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import { GAME_BUTTONS } from './game-buttons.js';
-import { setPlayerHandsCounter } from './game-hands.js';
-import { startGame } from './start-game.js';
+import { GAME_BUTTONS } from './game/game-buttons.js';
+import { setPlayerHandsCounter } from './game/game-hands.js';
+import { startGame } from './game/start-game.js';
 
 GAME_BUTTONS.start.addEventListener('click', startGame, { once: true });
 setPlayerHandsCounter(() => 1);
