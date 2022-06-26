835af8d715ee8ff5764a219269150eb3cb06465e
hdoc <blueguitarist1520@gmail.com>
2022-06-25 19:33:48 -0500

    Refactor(card-data.js): Changed data attribute used for card suit

--- src/js/card-data.js
+++ src/js/card-data.js
@@ -31,5 +31,5 @@ export const setCardData = (cardId, isToChange) => {
   // Displaying card info
   qsa('.card__info', $card).forEach((info) => (info.innerText = cardInfo));
   qs('.card__suit', $card).textContent = SUITS[suit];
-  $card.dataset.suitColor = suit.match(/^H|D$/) ? 'red' : 'black';
+  $card.dataset.suit = suit;
 };
