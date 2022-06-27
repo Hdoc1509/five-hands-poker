4ed488a61b2a15206463e6eda9d36781abe22142
hdoc <blueguitarist1520@gmail.com>
2022-06-27 18:02:45 -0500

    Add(hand-points.js): Added object with hands points

---
+++ src/js/game/hand-points.js
@@ -0,0 +1,13 @@
+export const HAND_POINTS = Object.freeze({
+  nothing: 0,
+  'high-card': 1,
+  pair: 2,
+  'two-pair': 5,
+  'three-of-kind': 10,
+  straight: 15,
+  flush: 20,
+  'full-house': 25,
+  'four-of-kind': 50,
+  'straight-flush': 75,
+  'royal-flush': 100,
+});
