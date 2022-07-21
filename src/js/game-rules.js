dfac1d9859036abb24ea51c26e37f7956fc282ae
hdoc <blueguitarist1520@gmail.com>
2022-07-21 18:31:36 -0500

    Feat: Prevent brody scroll when rules modal is open

--- src/js/game-rules.js
+++ src/js/game-rules.js
@@ -2,17 +2,20 @@ import { qs } from './utils/dom.js';
 
 const $dialogOpenBtn = qs('.dialog-open-button');
 const $dialogCloseBtn = qs('.dialog-rules__close');
+const $body = document.body;
 
 /** @type {HTMLDialogElement} */
 const $dialogRules = qs('.dialog-rules');
 
 const hideRules = () => {
   $dialogRules.close();
+  $body.classList.remove('no-scroll');
   $dialogOpenBtn.classList.remove('hidden');
 };
 
 const showRules = () => {
   $dialogRules.showModal();
+  $body.classList.add('no-scroll');
   $dialogOpenBtn.classList.add('hidden');
   $dialogCloseBtn.addEventListener('click', hideRules);
 };
