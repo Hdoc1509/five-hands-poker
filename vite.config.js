3d400e5312d5b9e1f8df6ebaf71cb2058e88c692
hdoc <blueguitarist1520@gmail.com>
2022-07-24 13:07:23 -0500

    Package: Add 'vite-plugin-html' package

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   esbuild: {
     target: 'es6',
     supported: { arrow: false },
   },
+  plugins: [createHtmlPlugin({ minify: true })],
 });
