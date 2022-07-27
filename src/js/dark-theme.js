b9c5222f954e6c52820d947d481c7a00b2836e82
hdoc <blueguitarist1520@gmail.com>
2022-07-27 18:39:45 -0500

    Refactor: Add new function saveTheme()

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,15 +1,15 @@
 import { gid } from './utils/dom';
 
 const $body = document.body;
-const ls = localStorage;
+const saveTheme = (theme) => localStorage.setItem('theme', theme);
 
 gid('btn-theme').addEventListener('click', () => {
   if ($body.classList.contains('dark-mode')) {
     $body.classList.remove('dark-mode');
-    ls.setItem('theme', 'light');
+    saveTheme('light');
     return;
   }
 
   $body.classList.add('dark-mode');
-  ls.setItem('theme', 'dark');
+  saveTheme('dark');
 });
