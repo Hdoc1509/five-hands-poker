4fa15eb308119be9c28b78c9892ee01fb6a9f8f9
hdoc <blueguitarist1520@gmail.com>
2022-07-29 16:34:23 -0500

    Feat: Use type assertion in game-version.ts

--- src/game-version.ts
+++ src/game-version.ts
@@ -1,9 +1,9 @@
 import { gid } from './utils/dom';
 import { version } from '../package.json';
 
-const $version = gid('game-version');
+const $version = gid('game-version') as HTMLAnchorElement;
 const releaseURL = 'https://github.com/Hdoc1509/five-hands-poker/releases/tag';
 const currentVersion = `v${version}`;
 
-$version!.setAttribute('href', `${releaseURL}/${currentVersion}`);
-$version!.textContent = `${currentVersion}`;
+$version.setAttribute('href', `${releaseURL}/${currentVersion}`);
+$version.textContent = `${currentVersion}`;
